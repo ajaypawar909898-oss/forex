14a3ae8110731e4b0b16a9232afbf2e3536672c4
ajaypawar909898-oss <ajaypawar909898@gmail.com>
2026-02-09 12:50:12 +0530

    dist file added

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ import usersRoutes from "./src/routes/users.routes.js";
 import tradeRoutes from "./src/routes/trade.routes.js";
 import bankRoutes from "./src/routes/bank.routes.js";
 import cryptoRoutes from "./src/routes/crypto.routes.js";
-import path from "path";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
